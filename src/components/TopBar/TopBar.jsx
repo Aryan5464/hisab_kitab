19d8aebd77845927d73dd7080496784639906da6
Aryan <aryanjainyk@gmail.com>
2024-11-02 22:32:23 +0530

    modal bug

--- src/components/TopBar/TopBar.jsx
+++ src/components/TopBar/TopBar.jsx
@@ -1,7 +1,7 @@
 import pro from '../../images/Logowhite.png';
 const TopBar = () => {
     return (
-        <div className="sm:hidden fixed w-svw h-14 bg-blueee top-0 flex justify-between items-center px-5 text-slate-100">
+        <div className="sm:hidden fixed w-svw h-14 bg-blueee top-0 flex justify-between items-center px-5 text-slate-100 shadow-lg shadow-white">
 
             <div className="flex justify-center cursor-default items-center rounded-md gap-1 p-1 select-none">
                 <img className="w-8" src={pro} alt="" />
